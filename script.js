108dc6ccab9de9f40a6b3fc383818aca8a2786a4
Jvml206 <lopesdrimachado2@gmail.com>
2025-08-02 10:01:12 -0400

    feat: teste da aula

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Curiosidade: A famosa capa de gordura da picanha é essencial para garantir maciez e sabor durante o churrasco!');
+});
